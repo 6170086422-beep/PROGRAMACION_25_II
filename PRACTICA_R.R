a11acc141d25df3aa73c22f23b95251efaa56725
unknown <6170086422@untrm.edu.pe>
2025-09-02 16:16:10 -0500

    clase práctica

---
+++ PRACTICA_R.R
@@ -0,0 +1,26 @@
+# MI PRIMERA PRACTICA CON R
+
+#alt + -
+X <- "Hola"
+Y <- "UNTRM"
+#f(X,Y) =X+Y
+# AYUDA: se pone encima del codigo y se digita F1 o Fn + F1
+paste(X,Y, )
+paste(X,Y)
+library(tidyverse)
+library(tidyverse)
+library(tidyverse)
+library(tidyverse)
+# pipe
+a1 <- "estudiante"
+1a <- "cocina"
+a_1 <- "objeto"
+a.1 <- "a"
+a-1 <- "b"
+notas <- c(16, 17, 19, 15)
+media <- mean(notas)
+media
+# ctrl + shift + m %>% 
+m <- c(16,17,19,15) %>% 
+  mean()
+m
